fe7df4d186a351780256d372996c2e5a81437f94
Mostafa Yasin <mostafayasin@paymob.com>
2023-02-18 19:29:40 +0200

    Added login feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import UserRegistrationView
+from django.contrib.auth.views import LoginView
 
 
-urlpatterns = [path("create", UserRegistrationView.as_view(), name="user_registration")]
+urlpatterns = [
+    path("create", UserRegistrationView.as_view(), name="user_registration"),
+    path("login", LoginView.as_view(template_name="login.html"), name="login"),
+]
