0dca4bb7d6a2df1a095f504bc8d73ee231693eaf
Mostafa Yasin <mostafayasin@paymob.com>
2023-02-18 23:04:35 +0200

    Added tests for login view

---
+++ accounts/tests/test_login.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+
+class TestLogin(TestCase):
+    def setUp(self) -> None:
+        self.response = self.client.get("/accounts/login")
+
+    def test_login_status_code_is_200(self) -> None:
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_login_view_has_login_link(self) -> None:
+        self.assertContains(self.response, "login", 2)
+
+    def test_login_view_doesnt_contain_logout_link(self) -> None:
+        self.assertNotContains(self.response, "logout")
