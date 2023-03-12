a27446e978dd3130ff96bd4272a7068742ba008e
Mostafa Yasin <mostafayasin@paymob.com>
2023-03-12 14:30:52 +0200

    feature/blog-post | WIP: Added a URL for create blog post view

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from blog.views import NewBlogView
 from blog.views import UpdateBlogView
+from blog.views import NewBlogPost
 
 
 urlpatterns = [
     path("create/", NewBlogView.as_view(), name="new-blog"),
     path("<int:pk>/edit/", UpdateBlogView.as_view(), name="edit-blog"),
+    path("<int:pk>/blog-posts/", NewBlogPost.as_view(), name="new-blog-post"),
 ]
