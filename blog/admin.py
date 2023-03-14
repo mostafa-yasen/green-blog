3bc59940aa0c8e8aa933d1336aa67421e3e3bca3
Mostafa Yasin <mostafayasin@paymob.com>
2023-03-14 11:03:50 +0200

    feature/admin-models | Registering models to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from blog.models import Blog
+from blog.models import BlogPost
+
+
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    pass
+
 
 # Register your models here.
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(BlogPost, BlogPostAdmin)
